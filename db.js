2c86a0f352316bfbac0875188c71872511b78962
fazeltamana <157019387+fazeltamana@users.noreply.github.com>
2025-10-10 01:48:42 +0600

    Database connection to render

--- db.js
+++ db.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
   port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false }
 });
 export default pool;
+
