36b4b7242bdafa4970fd024d95f9ba44d49121af
fazeltamana <157019387+fazeltamana@users.noreply.github.com>
2025-10-10 02:00:13 +0600

    Full name of the hostname included

--- db.js
+++ db.js
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST,         
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,     
   ssl: { rejectUnauthorized: false }
 });
-export default pool;
 
+export default pool;
